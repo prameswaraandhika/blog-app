ed52067930ad7f0f04a5997186e38c5dbb3cc5d0
Andhikap <prameswaara@gmail.com>
2024-01-30 23:35:55 +0700

    create post mapper

--- src/main/java/com/prameswaradev/blogapp/model/mapper/PostMapper.java
+++ src/main/java/com/prameswaradev/blogapp/model/mapper/PostMapper.java
@@ -7,7 +7,25 @@ public class PostMapper {
 
     public PostDto mappingToPostDto(Post post){
         return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .url(post.getUrl())
+                .description(post.getDescription())
+                .content(post.getContent())
+                .createdOn(post.getCreatedOn())
+                .updatedOn(post.getUpdatedOn())
+                .build();
+    }
 
+    public Post mappingToPost(PostDto postDto){
+        return Post.builder()
+                .id(postDto.getId())
+                .title(postDto.getTitle())
+                .url(postDto.getUrl())
+                .description(postDto.getDescription())
+                .content(postDto.getContent())
+                .createdOn(postDto.getCreatedOn())
+                .updatedOn(postDto.getUpdatedOn())
                 .build();
     }
 }
