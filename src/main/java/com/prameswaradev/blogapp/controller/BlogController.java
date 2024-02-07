149fb1c0fcf3e3d3e41d565ed1833ef4cf29c86b
Andhikap <prameswaara@gmail.com>
2024-02-07 14:18:25 +0700

    create view client blog

--- src/main/java/com/prameswaradev/blogapp/controller/BlogController.java
+++ src/main/java/com/prameswaradev/blogapp/controller/BlogController.java
@@ -6,20 +6,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
 public class BlogController {
 
-    private final PostService blogService;
+    private final PostService postService;
 
     @GetMapping(value = "/")
     public String viewBlogPosts(Model model){
-        List<PostDto> posts = blogService.findAllPosts();
+        List<PostDto> posts = postService.findAllPosts();
         model.addAttribute("posts", posts);
         return "blog/view_posts";
     }
+    
+    @GetMapping(value = "/post/{postUrl}")
+    public String viewBlogPostDetail(@PathVariable(value = "postUrl") String postUrl, Model model){
+        PostDto postDto = postService.findByUrl(postUrl);
+        model.addAttribute("post", postDto);
+        return "blog/blog_post";
+    }
+    
     
 }
