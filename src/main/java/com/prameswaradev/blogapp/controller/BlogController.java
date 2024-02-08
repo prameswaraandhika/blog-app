aca888f3def2d2cccdf3a4eaa8718f2a03e901b4
Andhikap <prameswaara@gmail.com>
2024-02-08 20:39:16 +0700

    create component search and method handler for search blog

--- src/main/java/com/prameswaradev/blogapp/controller/BlogController.java
+++ src/main/java/com/prameswaradev/blogapp/controller/BlogController.java
@@ -20,17 +20,24 @@ public class BlogController {
 
     @GetMapping(value = "/")
     public String viewBlogPosts(Model model){
-        List<PostDto> posts = postService.findAllPosts();
+        var posts = postService.findAllPosts();
         model.addAttribute("posts", posts);
         return "blog/view_posts";
     }
     
     @GetMapping(value = "/post/{postUrl}")
     public String viewBlogPostDetail(@PathVariable(value = "postUrl") String postUrl, Model model){
-        PostDto postDto = postService.findByUrl(postUrl);
+        var postDto = postService.findByUrl(postUrl);
         model.addAttribute("post", postDto);
         return "blog/blog_post";
     }
-    
+
+    @GetMapping(value = "/page/search")
+    public String searchPosts(@RequestParam(value = "query") String query,
+                              Model model){
+        var posts = postService.search(query);
+        model.addAttribute("posts", "posts");
+        return "blog/view_posts";
+    }
     
 }
